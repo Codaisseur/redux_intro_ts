646b7af07d759dcb714a99b1938c5347ebbf1861
Kelley van Evert <hello@kelleyvanevert.nl>
2020-09-13 21:16:24 +0200

    Dispatching actions to the store, getting the store's state, and subscribing to updates from the store

--- src/store.ts
+++ src/store.ts
@@ -4,3 +4,24 @@ import { myCountingReducer } from "./myCountingReducer";
 
 const store = createStore(myCountingReducer);
 console.log("Initial state of the store:", store.getState());
+
+store.subscribe(() => {
+  console.log("The state just changed to:", store.getState());
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 10,
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 5,
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 5,
+});
+
+console.log("final state:", store.getState());
